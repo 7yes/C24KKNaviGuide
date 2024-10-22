e1c056d460ebfc29c79ded3c3fae581ab0727ea5
yesmc <7yesid@gmail.com>
2024-10-22 12:06:45 -0500

    adecuando para nav con argumentos de home a detail

--- app/src/main/java/com/jesse/c24kknaviguide/core/screen/HomeScreen.kt
+++ app/src/main/java/com/jesse/c24kknaviguide/core/screen/HomeScreen.kt
@@ -5,16 +5,23 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(){
+    var data = rememberSaveable(){ mutableStateOf("") }
     Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally){
         Spacer(modifier = Modifier.weight(1f))
         Text(text = "Home Screen", fontSize = 30.sp)
+        Spacer(modifier = Modifier.weight(1f))
+        TextField(value = data.value, onValueChange = {data.value = it})
         Spacer(modifier = Modifier.weight(1f))
         Button(onClick = {  }) {
             Text(text = "Go to Detail")
